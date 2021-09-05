74bb2eac0b6c6342eada8fdd128e32309f430a71
evinceng <aslan.evin@gmail.com>
2021-09-05 22:37:10 +0200

    create pictorial proof for one signal low and high scored users

--- runExperiment_PictExamination.py
+++ runExperiment_PictExamination.py
@@ -8,16 +8,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import Utils
 from datetime import datetime
+import os.path
+from pathlib import Path
 
-selectedFactor = 4
+factorScoresFileName = "Data/MMEM_Scores/mmem_C1_argmin_F_df.csv"
+selectedFactor = 'F1'
 selectedContent = 'C1'
 
-lowFactorUserIDs = [14,16,17,10] #tobii [14,16,17,10] # empatica, shimmer pupillabs [1,2,3,4]
-highFactorUserIDs = [50,51,52,53] # empattica, shimmer [5,6,7,8] # pupillabs [33,34,35,36] #tobii
+userSensorContentFileName =  "Data/userContentSensorDict.csv"
 
-sensorID = 3
-signalName = 'pc_x' #'diameter' # 'EDA'
-sensorFileNameExt = 'pupilCenter_left_eye'#'left_eye_2d' #EDA
+# lowFactorUserIDs = [14,16,17,10] #tobii [14,16,17,10] # empatica, shimmer pupillabs [1,2,3,4]
+# highFactorUserIDs = [50,51,52,53] # empattica, shimmer [5,6,7,8] # pupillabs [33,34,35,36] #tobii
+
+sensorID = 1
+sensorFileNameExt = 'ACC'# 'pupilCenter_left_eye'#'left_eye_2d' #EDA
+signalName = 'AccX' #'pc_x' #'diameter' # 'EDA'
 
 outputFolderName = "PictorialProof/"
 
@@ -26,7 +31,7 @@ usersDictFileName = "Data/usersDict.xlsx"
 
 sensors = {1:'empatica', 2:'shimmer', 3:'tobii', 4:'pupilLabs'}
 
-signals = ['AccX', 'AccY', 'AccZ', 'EDA', 'BVP', 'IBI', 'HR',
+signals = ['AccX', 'AccY', 'AccZ', 'EDA', 'BVP', 'HR',
            'Accel_LN_X_m/(s^2)', 'Accel_LN_Y_m/(s^2)',
            'Accel_LN_Z_m/(s^2)', 'GSR_Range_no_units.1',
            'GSR_Skin_Conductance_microSiemens', 'GSR_Skin_Conductance_uS.1',
@@ -107,8 +112,8 @@ def generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(lowFactorUserIDs,
     fig, ax = plt.subplots(2, subPlotCount) #, sharey=True
     fig.suptitle(sensors[sensorID] +' ' + sensorFileNameExt +' ' + signalName + ' ' + selectedContent)
     
-    ax[0][0].set_title('Low score F' + str(selectedFactor))
-    ax[1][0].set_title('High score F' + str(selectedFactor))
+    ax[0][0].set_title('Low score ' + selectedFactor)
+    ax[1][0].set_title('High score ' + selectedFactor)
     
     # plot the first row of subplots
     generateSignalSubplots(ax, 0, lowFactorUserIDs, sensorID, signalName, sensorFileNameExt)    
@@ -121,19 +126,51 @@ def generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(lowFactorUserIDs,
     generateAdVidLines(ax, 1, usersDictFileName, highFactorUserIDs, selectedContent) 
     
     now = datetime.now().strftime("%Y_%m_%d-%H-%M-%S")
-    saveFileFig = outputFolderName + 'F' + str(selectedFactor) + '_' + sensors[sensorID] + '_' + signalName + '_' + now
+    saveFileFig = outputFolderName + selectedFactor + '_' + sensors[sensorID] + '_' + signalName + '_' + now
     plt.savefig(saveFileFig +'.jpg')
     Utils.writePickleFile(fig, saveFileFig)
     # pickle.dump(fig, open(saveFileFig +'.pickle', 'wb')) # This is for Python 3 - py2 may need `file` instead of `open`
     plt.show()
     
 
+
+def getLowAndHighFactorUserIDS(userIDList, factorScoresFileName, selectedFactor, numberOfUsers=4):
+    scores_df = pd.read_csv(factorScoresFileName)
+    scores_df = scores_df.loc[scores_df['userID'].isin(userIDList)] 
+    sorted_df = scores_df.sort_values(selectedFactor)
+    
+    # print(sorted_df['userID'][:numberOfUsers])
+    # print(sorted_df['userID'][-numberOfUsers:])
+    return sorted_df['userID'][:numberOfUsers], sorted_df['userID'][-numberOfUsers:]
+    
+
 # print(fileNameGenerator(1, 2))
 
+def getUsersSignalsOfOneContent(fileName, sensorID, contentID):
+    usersContent_df = pd.read_csv(fileName, encoding = "utf-8")
+    sensorContentStr = sensors[sensorID] + '_C' + str(contentID)
+    usersContent_df = usersContent_df.loc[usersContent_df[sensorContentStr] == 1]
+    # print(usersContent_df.head())
+    return usersContent_df['userID']
+
+
+# create the figures for one sensor-signal 
+
+filteredUserList = getUsersSignalsOfOneContent(userSensorContentFileName, 1, 1)
+print(filteredUserList)
+lowFactorUserIDs, highFactorUserIDs = getLowAndHighFactorUserIDS(filteredUserList, factorScoresFileName, selectedFactor)
 generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(lowFactorUserIDs,
                                                             highFactorUserIDs,
                                                             sensorID,
                                                             signalName,
                                                             sensorFileNameExt)
-                                                           
+
+rootFolder = "PictorialProof/"
+
+def createFiguresForAll(rootFolder, selectedFactor, sensors, signals):
+    for sensorID, sensor in sensors.items():
+        Path(rootFolder + "C" + str(selectedFactor) + '/' + sensor).mkdir(parents=True, exist_ok=True) # will not change directory if exists        
+        
+
+# createFiguresForAll(rootFolder, 1, sensors, signals)                                                   
 # Utils.loadFigFromPickleFile(outputFolderName + 'F1_empatica_EDA_2021_08_25-12-24-41.pickle')                                                          
