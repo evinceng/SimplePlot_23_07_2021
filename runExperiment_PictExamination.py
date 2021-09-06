ec5b5ee7aeac7aa644c1887d5e7210aa2be38158
evinceng <aslan.evin@gmail.com>
2021-09-06 08:40:47 +0200

    running all signals for one content and one factor

--- runExperiment_PictExamination.py
+++ runExperiment_PictExamination.py
@@ -29,31 +29,68 @@ outputFolderName = "PictorialProof/"
 rootFolder = "C:/Users/evinao/Documents/GitHub/SimplePlot_23_07_2021/output/user"
 usersDictFileName = "Data/usersDict.xlsx"
 
-sensors = {1:'empatica', 2:'shimmer', 3:'tobii', 4:'pupilLabs'}
+empaticaSignals = { 'ACC':['AccX', 'AccY', 'AccZ'], 'EDA':['EDA'], 'BVP':['BVP'], 'HR':['HR']}
 
-signals = ['AccX', 'AccY', 'AccZ', 'EDA', 'BVP', 'HR',
-           'Accel_LN_X_m/(s^2)', 'Accel_LN_Y_m/(s^2)',
-           'Accel_LN_Z_m/(s^2)', 'GSR_Range_no_units.1',
+shimmerSignals = {'':['Accel_LN_X_m/(s^2)', 'Accel_LN_Y_m/(s^2)',
+           'Accel_LN_Z_m/(s^2)', #'GSR_Range_no_units.1', #I don't have it since not resampled includes nounits 
            'GSR_Skin_Conductance_microSiemens', 'GSR_Skin_Conductance_uS.1',
            'Pressure_BMP280_kPa', 'PPG_A12_mV',
            'Temperature_BMP280_Degrees Celsius', 'GSR_Skin_Resistance_kOhms',
            'Gyro_Z_deg/s', 'Gyro_X_deg/s', 'Gyro_Y_deg/s',
-           'Accel_WR_Z_m/(s^2)', 'Accel_WR_Y_m/(s^2)', 'Accel_WR_X_m/(s^2)',
-           'norm_pos_x', 'norm_pos_y', 'diameter', 'ellipse_angle',
-           'circle_3d_normal_x', 'circle_3d_normal_y', 'circle_3d_normal_z',
-           'norm_pos_x', 'norm_pos_y', 'gaze_point_3d_x', 'gaze_point_3d_y',
+           'Accel_WR_Z_m/(s^2)', 'Accel_WR_Y_m/(s^2)', 'Accel_WR_X_m/(s^2)']}
+
+pupillabsSignals = {'left_eye_2d':['norm_pos_x', 'norm_pos_y', 'diameter', 'ellipse_angle'],
+                    'left_eye_3d':['norm_pos_x', 'norm_pos_y', 'diameter', 'ellipse_angle',
+           'circle_3d_normal_x', 'circle_3d_normal_y', 'circle_3d_normal_z'],
+                    'right_eye_2d':['norm_pos_x', 'norm_pos_y', 'diameter', 'ellipse_angle'],
+                    'right_eye_3d':['norm_pos_x', 'norm_pos_y', 'diameter', 'ellipse_angle',
+           'circle_3d_normal_x', 'circle_3d_normal_y', 'circle_3d_normal_z'],
+                    'gaze': ['norm_pos_x', 'norm_pos_y', 'gaze_point_3d_x', 'gaze_point_3d_y',
            'gaze_point_3d_z', 'gaze_normal0_x', 'gaze_normal0_y',
            'gaze_normal0_z', 'gaze_normal1_x', 'gaze_normal1_y',
-           'gaze_normal1_z', 'ac_x', 'ac_y', 'ac_z', 'gd_x', 'gd_y', 'gd_z',
-           'gp_x', 'gp_y', 'gp_latency', 'gp3d_x', 'gp3d_y', 'gp3d_z',
-           'gy_x', 'gy_y', 'gy_z', 'pc_x', 'pc_y', 'pc_z', 'diameter']
+           'gaze_normal1_z']}
+           
+tobiiSignals = {'accelerometer': ['ac_x', 'ac_y', 'ac_z'], 
+                'gazeDirection_left_eye' : ['gd_x', 'gd_y', 'gd_z'],
+                'gazeDirection_right_eye' : ['gd_x', 'gd_y', 'gd_z'],
+           'gazePosition': ['gp_x', 'gp_y', 'gp_latency'],
+           'gazePosition3D': ['gp3d_x', 'gp3d_y', 'gp3d_z'],
+           'gyroscope' :['gy_x', 'gy_y', 'gy_z'], 
+           'pupilCenter_left_eye' :['pc_x', 'pc_y', 'pc_z'],
+           'pupilCenter_right_eye' :['pc_x', 'pc_y', 'pc_z'],
+           'pupilDim_left_eye':['diameter'],
+           'pupilDim_right_eye':['diameter']}
+
+sensors = {1:['empatica', empaticaSignals] , 2:['shimmer', shimmerSignals],
+           3:['tobii',tobiiSignals],  4:['pupillabs', pupillabsSignals]}
+
+
+
+# sensors = {1:'empatica', 2:'shimmer', 3:'tobii', 4:'pupilLabs'}
+
+# signals = ['AccX', 'AccY', 'AccZ', 'EDA', 'BVP', 'HR',
+#            'Accel_LN_X_m/(s^2)', 'Accel_LN_Y_m/(s^2)',
+#            'Accel_LN_Z_m/(s^2)', 'GSR_Range_no_units.1',
+#            'GSR_Skin_Conductance_microSiemens', 'GSR_Skin_Conductance_uS.1',
+#            'Pressure_BMP280_kPa', 'PPG_A12_mV',
+#            'Temperature_BMP280_Degrees Celsius', 'GSR_Skin_Resistance_kOhms',
+#            'Gyro_Z_deg/s', 'Gyro_X_deg/s', 'Gyro_Y_deg/s',
+#            'Accel_WR_Z_m/(s^2)', 'Accel_WR_Y_m/(s^2)', 'Accel_WR_X_m/(s^2)',
+#            'norm_pos_x', 'norm_pos_y', 'diameter', 'ellipse_angle',
+#            'circle_3d_normal_x', 'circle_3d_normal_y', 'circle_3d_normal_z',
+#            'norm_pos_x', 'norm_pos_y', 'gaze_point_3d_x', 'gaze_point_3d_y',
+#            'gaze_point_3d_z', 'gaze_normal0_x', 'gaze_normal0_y',
+#            'gaze_normal0_z', 'gaze_normal1_x', 'gaze_normal1_y',
+#            'gaze_normal1_z', 'ac_x', 'ac_y', 'ac_z', 'gd_x', 'gd_y', 'gd_z',
+#            'gp_x', 'gp_y', 'gp_latency', 'gp3d_x', 'gp3d_y', 'gp3d_z',
+#            'gy_x', 'gy_y', 'gy_z', 'pc_x', 'pc_y', 'pc_z', 'diameter']
 
 mmaesFactors = {1:'AE', 2:'RE', 3:'AA', 4:'PI'}
 
 # print(mmaesFactors[1])
 
 def fileNameGenerator(userID, sensorID, sensorFileNameExt = ""):
-    fileNameStr = rootFolder + str(userID) + "/Resampled/uID-" + str(userID) + "_" + sensors[sensorID]
+    fileNameStr = rootFolder + str(userID) + "/Resampled/uID-" + str(userID) + "_" + sensors[sensorID][0]
     if sensorFileNameExt:
         fileNameStr = fileNameStr + '_'  + sensorFileNameExt + '_resampled.csv'
     else:
@@ -101,7 +138,8 @@ def generateAdVidLines(ax, axIndex, usersDictFileName,lowOrHighFactorUserIDs,sel
         ax[axIndex][subPlotId].axvline(x=ad[3], c='g')
         subPlotId = subPlotId + 1
 
-def generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(lowFactorUserIDs,
+def generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(contentID, 
+                                                               lowFactorUserIDs,
                                                                highFactorUserIDs,
                                                                sensorID,
                                                                signalName,
@@ -110,7 +148,7 @@ def generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(lowFactorUserIDs,
     subPlotCount = max(len(lowFactorUserIDs), len(highFactorUserIDs))
     #take min and max 
     fig, ax = plt.subplots(2, subPlotCount) #, sharey=True
-    fig.suptitle(sensors[sensorID] +' ' + sensorFileNameExt +' ' + signalName + ' ' + selectedContent)
+    fig.suptitle(sensors[sensorID][0] +' ' + sensorFileNameExt +' ' + signalName + ' ' + selectedContent)
     
     ax[0][0].set_title('Low score ' + selectedFactor)
     ax[1][0].set_title('High score ' + selectedFactor)
@@ -126,11 +164,17 @@ def generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(lowFactorUserIDs,
     generateAdVidLines(ax, 1, usersDictFileName, highFactorUserIDs, selectedContent) 
     
     now = datetime.now().strftime("%Y_%m_%d-%H-%M-%S")
-    saveFileFig = outputFolderName + selectedFactor + '_' + sensors[sensorID] + '_' + signalName + '_' + now
+    if '/' in signalName:
+        signalName = signalName.replace('/','-')
+    elif '.' in signalName:
+        signalName = signalName.replace('.','-')
+    saveFileFig = outputFolderName + contentID + '/' + sensors[sensorID][0] + '/' + selectedFactor + '_' + sensors[sensorID][0] + '_' + sensorFileNameExt + '_' + signalName + '_' + now
     plt.savefig(saveFileFig +'.jpg')
     Utils.writePickleFile(fig, saveFileFig)
     # pickle.dump(fig, open(saveFileFig +'.pickle', 'wb')) # This is for Python 3 - py2 may need `file` instead of `open`
-    plt.show()
+    plt.close(fig)
+    # plt.show()
+    
     
 
 
@@ -148,7 +192,7 @@ def getLowAndHighFactorUserIDS(userIDList, factorScoresFileName, selectedFactor,
 
 def getUsersSignalsOfOneContent(fileName, sensorID, contentID):
     usersContent_df = pd.read_csv(fileName, encoding = "utf-8")
-    sensorContentStr = sensors[sensorID] + '_C' + str(contentID)
+    sensorContentStr = sensors[sensorID][0] + '_' + str(contentID)
     usersContent_df = usersContent_df.loc[usersContent_df[sensorContentStr] == 1]
     # print(usersContent_df.head())
     return usersContent_df['userID']
@@ -156,21 +200,32 @@ def getUsersSignalsOfOneContent(fileName, sensorID, contentID):
 
 # create the figures for one sensor-signal 
 
-filteredUserList = getUsersSignalsOfOneContent(userSensorContentFileName, 1, 1)
-print(filteredUserList)
-lowFactorUserIDs, highFactorUserIDs = getLowAndHighFactorUserIDS(filteredUserList, factorScoresFileName, selectedFactor)
-generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(lowFactorUserIDs,
+# filteredUserList = getUsersSignalsOfOneContent(userSensorContentFileName, 1, 1)
+# print(filteredUserList)
+# lowFactorUserIDs, highFactorUserIDs = getLowAndHighFactorUserIDS(filteredUserList, factorScoresFileName, selectedFactor)
+# generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(lowFactorUserIDs,
+#                                                             highFactorUserIDs,
+#                                                             sensorID,
+#                                                             signalName,
+#                                                             sensorFileNameExt)
+
+pictOutputFolder = "PictorialProof/"
+
+def createFiguresForAll(pictOutputFolder, selectedFactor, contentID, sensors, userSensorContentFileName, factorScoresFileName):
+    for sensorID, sensor in sensors.items():
+        Path(pictOutputFolder + contentID + '/' + sensor[0]).mkdir(parents=True, exist_ok=True) # will not change directory if exists
+        filteredUserList = getUsersSignalsOfOneContent(userSensorContentFileName, sensorID, contentID)
+        print(filteredUserList)
+        lowFactorUserIDs, highFactorUserIDs = getLowAndHighFactorUserIDS(filteredUserList, factorScoresFileName, selectedFactor)
+        
+        for sensorFileNameExt, signalList in sensor[1].items():
+            for signalName in signalList:
+                generateSubPlotsofOneSignalOfMultipleUsersWithAdVideoTimes(contentID, lowFactorUserIDs,
                                                             highFactorUserIDs,
                                                             sensorID,
                                                             signalName,
-                                                            sensorFileNameExt)
-
-rootFolder = "PictorialProof/"
-
-def createFiguresForAll(rootFolder, selectedFactor, sensors, signals):
-    for sensorID, sensor in sensors.items():
-        Path(rootFolder + "C" + str(selectedFactor) + '/' + sensor).mkdir(parents=True, exist_ok=True) # will not change directory if exists        
+                                                            sensorFileNameExt)        
         
 
-# createFiguresForAll(rootFolder, 1, sensors, signals)                                                   
+createFiguresForAll(pictOutputFolder, 'F1', 'C1', sensors, userSensorContentFileName, factorScoresFileName)                                                   
 # Utils.loadFigFromPickleFile(outputFolderName + 'F1_empatica_EDA_2021_08_25-12-24-41.pickle')                                                          
