09b7a035f395ba3650df86a4109e02b9638b2aa4
evinceng <aslan.evin@gmail.com>
2021-09-17 10:09:06 +0200

    running 6 users pickle files added to pictExam

--- runExperiment_PictExamination.py
+++ runExperiment_PictExamination.py
@@ -26,7 +26,9 @@ signalName = 'AccX' #'pc_x' #'diameter' # 'EDA'
 
 outputFolderName = "PictorialProof/"
 
-rootFolder = "C:/Users/evinao/Documents/GitHub/SimplePlot_23_07_2021/output/user"
+
+rootFolder = "C:/Users/evinao/Dropbox (Lucami)/Lucami Team Folder/MMEM_Data/output/user"
+
 usersDictFileName = "Data/usersDict.xlsx"
 
 empaticaSignals = { 'ACC':['AccX', 'AccY', 'AccZ'], 'EDA':['EDA'], 'BVP':['BVP'], 'HR':['HR']}
@@ -227,7 +229,9 @@ def createFiguresForAll(pictOutputFolder, selectedFactor, contentID, sensors, us
                                                             sensorFileNameExt)        
         
 
-# createFiguresForAll(pictOutputFolder, selectedFactor, selectedContent, sensors, userSensorContentFileName, factorScoresFileName)    
+# createFiguresForAll(pictOutputFolder, selectedFactor, selectedContent, sensors, userSensorContentFileName, factorScoresFileName)  
+
+# 6 users  
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1/empatica/F1_empatica_ACC_AccX_2021_09_07-12-32-02.pickle')
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1/empatica/F1_empatica_ACC_AccY_2021_09_07-12-32-05.pickle')
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1/empatica/F1_empatica_ACC_AccZ_2021_09_07-12-32-08.pickle')
@@ -235,11 +239,45 @@ def createFiguresForAll(pictOutputFolder, selectedFactor, contentID, sensors, us
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1/empatica/F1_empatica_EDA_EDA_2021_09_07-12-32-11.pickle')
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1/empatica/F1_empatica_HR_HR_2021_09_07-12-32-17.pickle')
 
-
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Accel_LN_X_m-(s^2)_2021_09_07-12-32-26.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Accel_LN_Y_m-(s^2)_2021_09_07-12-32-35.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Accel_LN_Z_m-(s^2)_2021_09_07-12-32-44.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Accel_WR_X_m-(s^2)_2021_09_07-12-34-35.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Accel_WR_Y_m-(s^2)_2021_09_07-12-34-25.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Accel_WR_Z_m-(s^2)_2021_09_07-12-34-16.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Accel_WR_Z_m-(s^2)_2021_09_07-12-34-16.pickle')
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__GSR_Skin_Conductance_microSiemens_2021_09_07-12-32-53.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__GSR_Skin_Conductance_uS-1_2021_09_07-12-33-03.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__GSR_Skin_Resistance_kOhms_2021_09_07-12-33-39.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Gyro_X_deg-s_2021_09_07-12-33-58.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Gyro_Y_deg-s_2021_09_07-12-34-07.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Gyro_Z_deg-s_2021_09_07-12-33-48.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__PPG_A12_mV_2021_09_07-12-33-21.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Pressure_BMP280_kPa_2021_09_07-12-33-12.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/shimmer/F1_shimmer__Temperature_BMP280_Degrees Celsius_2021_09_07-12-33-31.pickle')
+
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/pupillabs/F1_pupillabs_gaze_gaze_normal0_x_2021_09_07-12-40-10.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/pupillabs/F1_pupillabs_gaze_gaze_normal1_x_2021_09_07-12-40-34.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/pupillabs/F1_pupillabs_gaze_gaze_point_3d_x_2021_09_07-12-39-46.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/pupillabs/F1_pupillabs_gaze_norm_pos_x_2021_09_07-12-39-30.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/pupillabs/F1_pupillabs_left_eye_2d_diameter_2021_09_07-12-36-10.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/pupillabs/F1_pupillabs_left_eye_2d_ellipse_angle_2021_09_07-12-36-15.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/pupillabs/F1_pupillabs_left_eye_3d_circle_3d_normal_x_2021_09_07-12-37-15.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/pupillabs/F1_pupillabs_left_eye_3d_diameter_2021_09_07-12-36-52.pickle')
+
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_accelerometer_ac_x_2021_09_07-12-34-38.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_accelerometer_ac_y_2021_09_07-12-34-41.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_accelerometer_ac_z_2021_09_07-12-34-44.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_gazeDirection_left_eye_gd_x_2021_09_07-12-34-47.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_gazePosition_gp_latency_2021_09_07-12-35-11.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_gazePosition_gp_x_2021_09_07-12-35-05.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_gazePosition3D_gp3d_x_2021_09_07-12-35-14.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_gyroscope_gy_x_2021_09_07-12-35-24.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_pupilCenter_left_eye_pc_x_2021_09_07-12-35-33.pickle')
+# Utils.loadFigFromPickleFile(outputFolderName + 'C1/tobii/F1_tobii_pupilDim_left_eye_diameter_2021_09_07-12-35-53.pickle')
 
 
-                                               
+#4 users                                             
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1-4 users/empatica/F1_empatica_AccX_2021_09_06-00-29-59.pickle')
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1-4 users/empatica/F1_empatica_BVP_2021_09_06-00-30-06.pickle')   
 # Utils.loadFigFromPickleFile(outputFolderName + 'C1-4 users/empatica/F1_empatica_EDA_2021_09_06-00-30-05.pickle') 
